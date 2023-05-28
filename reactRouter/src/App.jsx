de83e6aa9dbc395e2efd0325d6f8f35c1c78ce7c
12jaime12 <12jaimejcc12@gmail.com>
2023-05-29 00:05:27 +0200

    react_router_practice

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import "./App.css";
+import { NavLink, Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>React Router v6 🧪</h1>
+      </header>
+      <div>
+        <nav>
+          <NavLink to="">Home</NavLink>
+          <NavLink to="heroes">Heroes</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactRouter/src/components/HeroeDetail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroeDetail = ({ heroe }) => {
+  return (
+    <>
+      <h1>name: {heroe.name}</h1>
+      <p>alias: {heroe.alias}</p>
+      <p>age: {heroe.age}</p>
+      <p>id: {heroe.id}</p>
+    </>
+  );
+};
+
+export default HeroeDetail;
