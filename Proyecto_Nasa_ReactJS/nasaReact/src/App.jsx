1966999dc36888e99b30affb4eefc0c4c7e9e72b
12jaime12 <12jaimejcc12@gmail.com>
2023-06-04 18:42:48 +0200

    nasa

---
+++ Proyecto_Nasa_ReactJS/nasaReact/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Figure from "./components/Figure";
+import axios from "axios";
+function App() {
+  const today = new Date(Date.now()).toISOString().slice(0, 10);
+
+  const [apod, setApod] = useState({});
+
+  const [date, setDate] = useState(today);
+
+  const NASA_URL = "https://api.nasa.gov/";
+
+  const NASA_API_KEY = "DRQe3BRfoZiL02ckiKqdvrncQCpLiVOCfBbzVV2G";
+
+  useEffect(() => {
+    const getApod = async () => {
+      const data = await axios.get(
+        `${NASA_URL}planetary/apod?date=${date}&api_key=${NASA_API_KEY}`
+      );
+      setApod(data.data);
+    };
+    getApod();
+  }, [date]);
+
+  const handleInput = (ev) => {
+    setDate(ev.target.value.toLocaleString());
+  };
+  return (
+    <div className="App">
+      <h1>Foto Astrologica del dia</h1>
+      <input type="date" id="photo-date" onChange={handleInput} />
+      {date > today ? (
+        <h2>Elige una fecha previa a la actual</h2>
+      ) : (
+        <Figure data={apod} />
+      )}
+      <div className="standard-dialog center">
+        <h3 className="dialog-text">
+          @lethamburn - 2022 -{" "}
+          <a href="https://api.nasa.gov/">https://api.nasa.gov/</a>
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
